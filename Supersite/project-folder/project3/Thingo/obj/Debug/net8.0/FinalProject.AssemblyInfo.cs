052949d473dc1e7f9539dcedb4904c590376f3cc
fpom1 <jen21017@byui.edu>
2026-02-04 23:49:13 -0700

    add logo
    Logo added
    index.html line 15
    index.css line 32-34

--- Supersite/project-folder/project3/Thingo/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ Supersite/project-folder/project3/Thingo/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc1f4f8dd9b6819cf28d012db44badf29e7b4f63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2168cf2a661dc3a37e7455ed868b7adedb87200f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
